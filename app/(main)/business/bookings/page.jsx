af87b9d840e509d25c052b87c9c02d1fe6c3cf18
DevDeepakKumarKhatri <deepak.kumar@tixertech.com>
2024-08-02 16:47:45 +0500

    Partial Completed bookings page

--- app/(main)/business/bookings/page.jsx
+++ app/(main)/business/bookings/page.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import BookingsTable from "../../../../components/sections/bussiness/bookings/BookingsTable";
 
-const page = () => {
+const Page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="flex flex-col">
+      <BookingsTable />
+    </div>
+  );
+};
 
-export default page
+export default Page;
---
+++ components/sections/bussiness/bookings/TableUpper.jsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useDispatch } from "react-redux";
+import SearchField from "../../../fields/SearchField";
+import React, { useEffect } from "react";
+import { setActivePage } from "../../../../redux/slices/headerSlice";
+import BagsFilter from "../../../dropdowns/BagsFilter";
+
+const TableUpper = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setActivePage("Bookings"));
+  }, [dispatch]);
+
+  return (
+    <div className="flex flex-col sm:flex-row sm:justify-between items-center mb-4 px-2 sm:px-4">
+      <p className="text-[16px] font-bold text-blackTwo hidden sm:block">
+        My Bags
+      </p>
+      <div className="flex flex-col sm:flex-row gap-2 w-full sm:w-auto">
+        <BagsFilter />
+        <SearchField placeholder={"Search Booking"} />
+      </div>
+    </div>
+  );
+};
+
+export default TableUpper;
