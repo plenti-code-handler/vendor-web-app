cdf32e1110e664168c94b543a42c30f28f8c2188
Shine <shine160700@gmail.com>
2026-02-23 12:26:56 +0530

    Made 2 col grid

--- components/sections/bussiness/bags/ItemTemplates.jsx
+++ components/sections/bussiness/bags/ItemTemplates.jsx
@@ -75,7 +75,7 @@ const ItemTemplates = () => {
                 </div>
             </div>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4">
+            <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-5 gap-4">
                 {templates.map((item) => (
                     <div
                         key={item.id}
