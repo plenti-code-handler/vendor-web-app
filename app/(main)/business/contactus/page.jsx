b7e709c9fa87ee795e1a45824b3265c2384b2811
Shine <shine160700@gmail.com>
2026-02-27 20:38:39 +0530

    Added seperate contactus page and added new sidebar menu style

---
+++ app/(main)/business/contactus/page.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+import React from "react";
+import ContactUsContent from "../../../../components/sections/bussiness/contactus/ContactUsContent";
+
+const ContactUsPage = () => {
+  return <ContactUsContent showBackButton={false} />;
+};
+
+export default ContactUsPage;
