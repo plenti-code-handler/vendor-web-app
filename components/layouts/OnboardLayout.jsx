4c5c9c65b0f33fc20d74a1947218cb1813b8de2f
Shine <shine160700@gmail.com>
2026-03-11 17:59:11 +0530

    Fixed terms modal scrolling issue

--- components/layouts/OnboardLayout.jsx
+++ components/layouts/OnboardLayout.jsx
@@ -348,7 +348,8 @@ export const OnboardLayout = ({ children }) => {
       <div className="flex flex-col lg:flex-row pt-5 pb-5 justify-between px-10">
         <AuthLeftContent />
 
-        <div className="flex flex-col w-full lg:w-[60%] bg-white lg:min-h-[85vh] lg:h-[95vh] max-h-[800px] rounded-[24px] shadow-lg overflow-hidden mt-10 lg:mt-20">
+        <div className="flex flex-col w-full lg:w-[60%] bg-white h-[85vh] lg:min-h-[85vh] lg:h-[95vh] max-h-[850px] rounded-[24px] shadow-lg overflow-hidden mt-10 lg:mt-20">
+
           {showTimeline && (
             <OnboardingTimeline currentStep={onboardingState.step} isApproved={vendorData?.account_approved} />
           )}
